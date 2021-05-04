63a61b3adb8b5ef1c795e3b4492c005fc9181bab
rynrn <rynrn8@gmail.com>
2021-05-05 01:40:17 +0300

    update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,8 +4,8 @@ import Layout from "../components/Layout/Layout"
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>דף לא נמצא</h1>
+    <p>הגעת לדף שכבר לא קיים</p>
   </Layout>
 )
 
